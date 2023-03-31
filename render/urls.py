8b4111a5877ab0819f4892c430cf71a8235d856d
jhoncabrera2050 <86067531+jhoncabrera2050@users.noreply.github.com>
2023-03-31 10:17:11 -0500

    cuarto commit

--- render/urls.py
+++ render/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from  . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('categoria/<int:categoria_id>/',views.categoria,name="categoria"),
 ]
--- render/views.py
+++ render/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from render.models import Post,Categoria
+# Create your views here.
 
-def index(request):
-    return render(request, 'render/index.html',{})
+
+def blog(request):
+    posts=Post.objects.all()
+    return render(request,'blog/blog.html',{"posts":posts})
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts=Post.objects.filter(categorias=categoria)
+    return render(request,"blog/categoria.html",{'categoria':categoria,"posts":posts})
