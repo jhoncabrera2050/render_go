d363cf0ea205206f0126502ed497e2b997e67f38
jhoncabrera2050 <86067531+jhoncabrera2050@users.noreply.github.com>
2023-03-31 16:11:02 -0500

    sexto commit

--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('render',include('render.urls')),
+    path('',include('render.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- render/views.py
+++ render/views.py
@@ -8,9 +8,9 @@ from render.models import Post,Categoria
 
 def blog(request):
     posts=Post.objects.all()
-    return render(request,'blog/blog.html',{"posts":posts})
+    return render(request,'render/blog.html',{"posts":posts})
 
 def categoria(request, categoria_id):
     categoria = Categoria.objects.get(id=categoria_id)
     posts=Post.objects.filter(categorias=categoria)
-    return render(request,"blog/categoria.html",{'categoria':categoria,"posts":posts})
+    return render(request,"render/categoria.html",{'categoria':categoria,"posts":posts})
